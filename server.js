9ef9df009c5a3f77c229511ddf4aba9f791e38d6
Andrea Fiori <a.fiori@geosystems.it>
2021-01-13 17:17:28 +0100

    Updated coding standards and README

--- server.js
+++ server.js
@@ -2,28 +2,28 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var fs = require('fs');
-var swig  = require('swig');
+var swig = require('swig');
 
 var MessageValidator = require("./model/MessageValidator");
 
 function twoDigits(d) {
-	if(0 <= d && d < 10) return "0" + d.toString();
-	if(-10 < d && d < 0) return "-0" + (-1*d).toString();
-	return d.toString();
+  if (0 <= d && d < 10) return "0" + d.toString();
+  if (-10 < d && d < 0) return "-0" + (-1 * d).toString();
+  return d.toString();
 }
 
-Date.prototype.toMysqlFormat = function() {
-	return this.getUTCFullYear() + "-" + twoDigits(1 + this.getUTCMonth()) + "-" + twoDigits(this.getUTCDate()) + " " + twoDigits(this.getUTCHours()) + ":" + twoDigits(this.getUTCMinutes()) + ":" + twoDigits(this.getUTCSeconds());
+Date.prototype.toMysqlFormat = function () {
+  return this.getUTCFullYear() + "-" + twoDigits(1 + this.getUTCMonth()) + "-" + twoDigits(this.getUTCDate()) + " " + twoDigits(this.getUTCHours()) + ":" + twoDigits(this.getUTCMinutes()) + ":" + twoDigits(this.getUTCSeconds());
 };
 
-Date.prototype.toMysqlTimeStamp = function() {
-	return this.getUTCFullYear() + "_" + twoDigits(1 + this.getUTCMonth()) + "_" + twoDigits(this.getUTCDate()) + "-" + twoDigits(this.getUTCHours()) + "" + twoDigits(this.getUTCMinutes()) + "" + twoDigits(this.getUTCSeconds());
+Date.prototype.toMysqlTimeStamp = function () {
+  return this.getUTCFullYear() + "_" + twoDigits(1 + this.getUTCMonth()) + "_" + twoDigits(this.getUTCDate()) + "-" + twoDigits(this.getUTCHours()) + "" + twoDigits(this.getUTCMinutes()) + "" + twoDigits(this.getUTCSeconds());
 };
 
-io.on('connection', function(socket) {
-    socket.on('container message', function(msg) {
-        io.emit('container message', msg);
-    });
+io.on('connection', function (socket) {
+  socket.on('container message', function (msg) {
+    io.emit('container message', msg);
+  });
 });
 
 app.engine('html', swig.renderFile);
@@ -31,92 +31,85 @@ app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 app.set('view cache', false);
-swig.setDefaults({ cache: false });
 
-app.use(function(req, res, next) {
+swig.setDefaults({
+  cache: false
+});
 
-    var contentType = req.headers['content-type'] || '', mime = contentType.split(';')[0];
+app.use(function (req, res, next) {
 
-    if (mime != 'text/plain') {
-        return next();
-    }
+  var contentType = req.headers['content-type'] || '';
+  var mime = contentType.split(';')[0];
 
-    var data = '';
-    req.setEncoding('utf8');
-    req.on('data', function(chunk) {
-        data += chunk;
+  if (mime !== 'text/plain') {
+    return next();
+  }
+
+  var data = '';
+  req.setEncoding('utf8');
+  req.on('data', function (chunk) {
+    data += chunk;
+  });
+  req.on('end', function () {
+    req.rawBody = data;
+    next();
+  });
+});
+
+app.get('/', function (req, res) {
+
+  // var dir = './app/messages';
+  // var fileContents = [];
+  // TODO: read list of json files, fill HTML table with data rows
+
+  res.render('index', {
+    appTitle: 'Market trade processor',
+    tableTitle: 'Messages',
+    fileContents: fileContents
+  });
+});
+
+app.post('/api/messsage', function (req, res) {
+
+  var rawBody = req.rawBody;
+
+  try {
+    var MsgValidator = new MessageValidator();
+
+    MsgValidator.checkMessageIsNotEmpty(rawBody);
+    MsgValidator.decodeMessage(rawBody);
+    MsgValidator.validateUserId();
+    MsgValidator.validateCurrencyFrom();
+    MsgValidator.validateCurrencyTo();
+    MsgValidator.validateAmountSell();
+    MsgValidator.validateAmountBuy();
+    MsgValidator.validateRate();
+    MsgValidator.validateTimePlaced();
+    MsgValidator.validateOriginatingCountry();
+
+    io.emit('container message', MsgValidator.decodedMessage);
+
+    /* save files on file system */
+    var outputFilename = './app/messages/' + new Date().toMysqlTimeStamp() + '_msg.json';
+
+    fs.writeFile(outputFilename, JSON.stringify(MsgValidator.decodedMessage, null, 4), function (err) {
+      if (err) {
+        throw new Error('Error writing file on file system: ' + err);
+      }
     });
-    req.on('end', function() {
-        req.rawBody = data;
-        next();
-    });
-});
 
-app.get('/', function(req, res) {
+    res.statusCode = 200;
+    res.type('application/json');
+    return res.send('{"message": "The message has been sent"}');
 
-	var dir = './app/messages/';
-	var fileContents = [];
-
-	fs.readdir(dir,function(err, files){
-		if (err) throw err;
-
-		files.forEach(function(file){
-			fs.readFile(dir + file, 'utf-8', function(err,html) {
-				if (err) throw err;
-
-				fileContents.push(html);
-			});
-		});
-	});
-
-	res.render('index', {
-		appTitle: 'Market trade processor',
-		tableTitle: 'Messages',
-		fileContents: fileContents
-	});
-});
-
-app.post('/api/messsage', function(req, res) {
-
-	var rawBody = req.rawBody;
-
-	try {
-		var MsgValidator = new MessageValidator();
-
-		MsgValidator.checkMessageIsNotEmpty(rawBody);
-		MsgValidator.decodeMessage(rawBody);
-		MsgValidator.validateUserId();
-		MsgValidator.validateCurrencyFrom();
-		MsgValidator.validateCurrencyTo();
-		MsgValidator.validateAmountSell();
-		MsgValidator.validateAmountBuy();
-		MsgValidator.validateRate();
-		MsgValidator.validateTimePlaced();
-		MsgValidator.validateOriginatingCountry();
-
-		io.emit('container message', MsgValidator.decodedMessage);
-
-		/* save files on file system */
-		var outputFilename = './app/messages/'+new Date().toMysqlTimeStamp()+'_msg.json';
-
-		fs.writeFile(outputFilename, JSON.stringify(MsgValidator.decodedMessage, null, 4), function(err) {
-			if(err) {
-				throw new Error('Error writing file on file system: '+err);
-			}
-		});
-
-		res.statusCode = 200;
-		res.type('application/json');
-		return res.send('{"message": "The message has been sent"}');
-
-	} catch (e) {
-		res.statusCode = 400;
-		res.type('application/json');
-		return res.send('{"message": "'+ e.message+'"}');
-	}
+  } catch (e) {
+    res.statusCode = 400;
+    res.type('application/json');
+    return res.send('{"message": "' + e.message + '"}');
+  }
 
 });
 
-http.listen(3000, function() {
-	console.log('listening on *:3000');
+http.listen(3001, function () {
+  console.log('listening on *:3001');
 });
--- test/MessageValidatorTest.js
+++ test/MessageValidatorTest.js
@@ -2,124 +2,124 @@ var assert = require("assert");
 
 MessageValidator = require("../model/MessageValidator.js");
 
-describe('MessageValidator tests', function() {
+describe('MessageValidator tests', function () {
 
-	beforeEach(function(done) {
-		this.testMessageValidator = new MessageValidator();
-		done();
-	});
+  beforeEach(function (done) {
+    this.testMessageValidator = new MessageValidator();
+    done();
+  });
 
-	afterEach(function(done) {
-		done();
-	});
+  afterEach(function (done) {
+    done();
+  });
 
-	describe('Message Validator string decode and validation', function() {
+  describe('Message Validator string decode and validation', function () {
 
-		it('decode a valid JSON string', function() {
-			assert.notDeepEqual(
-				this.testMessageValidator.decodeMessage('{"name": "John"}'),
-				'{"name": "John"}'
-			);
-		});
+    it('decode a valid JSON string', function () {
+      assert.notDeepEqual(
+        this.testMessageValidator.decodeMessage('{"name": "John"}'),
+        '{"name": "John"}'
+      );
+    });
 
-		it('should get the userId from the decoded JSON string', function() {
-			this.testMessageValidator.decodeMessage('{"userId": "123"}');
+    it('should get the userId from the decoded JSON string', function () {
+      this.testMessageValidator.decodeMessage('{"userId": "123"}');
 
-			assert.throws(
-				this.testMessageValidator.validateUserId(),
-				Error
-			);
-		});
+      assert.throws(
+        this.testMessageValidator.validateUserId(),
+        Error
+      );
+    });
 
-		it('should get the userId from the decoded JSON string', function() {
-			this.testMessageValidator.decodeMessage('{"userId": "123"}');
+    it('should get the userId from the decoded JSON string', function () {
+      this.testMessageValidator.decodeMessage('{"userId": "123"}');
 
-			assert.throws(
-				this.testMessageValidator.validateUserId(),
-				Error
-			);
-		});
+      assert.throws(
+        this.testMessageValidator.validateUserId(),
+        Error
+      );
+    });
 
-		it('assertDecodedMessage should throw Error exception', function() {
-			this.testMessageValidator.decodeMessage('{"userId": "123"}');
+    it('assertDecodedMessage should throw Error exception', function () {
+      this.testMessageValidator.decodeMessage('{"userId": "123"}');
 
-			assert.throws(
-				this.testMessageValidator.assertDecodedMessage(),
-				Error
-			);
-		});
+      assert.throws(
+        this.testMessageValidator.assertDecodedMessage(),
+        Error
+      );
+    });
 
-		it('userId value should be in the JSON string', function() {
-			this.testMessageValidator.decodeMessage('{"userId": "123"}');
+    it('userId value should be in the JSON string', function () {
+      this.testMessageValidator.decodeMessage('{"userId": "123"}');
 
-			assert.throws(
-				this.testMessageValidator.validateUserId(),
-				Error
-			);
-		});
+      assert.throws(
+        this.testMessageValidator.validateUserId(),
+        Error
+      );
+    });
 
-		it('currencyFrom value should be in the JSON string', function() {
-			this.testMessageValidator.decodeMessage('{"currencyFrom": "USD"}');
+    it('currencyFrom value should be in the JSON string', function () {
+      this.testMessageValidator.decodeMessage('{"currencyFrom": "USD"}');
 
-			assert.throws(
-				this.testMessageValidator.validateCurrencyFrom(),
-				Error
-			);
-		});
+      assert.throws(
+        this.testMessageValidator.validateCurrencyFrom(),
+        Error
+      );
+    });
 
-		it('currencyTo value should be in the JSON string', function() {
-			this.testMessageValidator.decodeMessage('{"currencyTo": "EUR"}');
+    it('currencyTo value should be in the JSON string', function () {
+      this.testMessageValidator.decodeMessage('{"currencyTo": "EUR"}');
 
-			assert.throws(
-				this.testMessageValidator.validateCurrencyTo(),
-				Error
-			);
-		});
+      assert.throws(
+        this.testMessageValidator.validateCurrencyTo(),
+        Error
+      );
+    });
 
-		it('amountBuy value should be in the JSON string', function() {
-			this.testMessageValidator.decodeMessage('{"amountBuy": "1245"}');
+    it('amountBuy value should be in the JSON string', function () {
+      this.testMessageValidator.decodeMessage('{"amountBuy": "1245"}');
 
-			assert.throws(
-				this.testMessageValidator.validateAmountBuy(),
-				Error
-			);
-		});
+      assert.throws(
+        this.testMessageValidator.validateAmountBuy(),
+        Error
+      );
+    });
 
-		it('amountSell value should be in the JSON string', function() {
-			this.testMessageValidator.decodeMessage('{"amountSell": "1245"}');
+    it('amountSell value should be in the JSON string', function () {
+      this.testMessageValidator.decodeMessage('{"amountSell": "1245"}');
 
-			assert.throws(
-				this.testMessageValidator.validateAmountSell(),
-				Error
-			);
-		});
+      assert.throws(
+        this.testMessageValidator.validateAmountSell(),
+        Error
+      );
+    });
 
-		it('rate value should be in the JSON string', function() {
-			this.testMessageValidator.decodeMessage('{"rate": "0.4211"}');
+    it('rate value should be in the JSON string', function () {
+      this.testMessageValidator.decodeMessage('{"rate": "0.4211"}');
 
-			assert.throws(
-				this.testMessageValidator.validateRate(),
-				Error
-			);
-		});
+      assert.throws(
+        this.testMessageValidator.validateRate(),
+        Error
+      );
+    });
 
-		it('timePlaced value should be in the JSON string', function() {
-			this.testMessageValidator.decodeMessage('{"timePlaced": "10-APR-15 11:00:24"}');
+    it('timePlaced value should be in the JSON string', function () {
+      this.testMessageValidator.decodeMessage('{"timePlaced": "10-APR-15 11:00:24"}');
 
-			assert.throws(
-				this.testMessageValidator.validateTimePlaced(),
-				Error
-			);
-		});
+      assert.throws(
+        this.testMessageValidator.validateTimePlaced(),
+        Error
+      );
+    });
 
-		it('originatingCountry value should be in the JSON string', function() {
-			this.testMessageValidator.decodeMessage('{"originatingCountry": "IT"}');
+    it('originatingCountry value should be in the JSON string', function () {
+      this.testMessageValidator.decodeMessage('{"originatingCountry": "IT"}');
 
-			assert.throws(
-				this.testMessageValidator.validateOriginatingCountry(),
-				Error
-			);
-		});
-	});
+      assert.throws(
+        this.testMessageValidator.validateOriginatingCountry(),
+        Error
+      );
+    });
+  });
 
 });
